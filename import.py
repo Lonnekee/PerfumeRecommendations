9b84f351ee98b951f8fd8e2f646194f57c810c7b
Barbera de Mol <barberademol@MacBook-Air.local>
2020-11-18 11:46:37 +0100

    how to import the data and use it to locate specific tags

---
+++ import.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def load_data(file):
+    data = pd.read_csv(file)
+    data = data.set_index('Handle')
+
+    # Print full database
+    print(data)
+
+    # Get specific column
+    print(data['Title'])
+
+    # Search for all products with specific tag
+    print(data[data['Tags'].str.contains('ingr:amber') == True].index)
+
+def main():
+    data = load_data('filteredDatabase.csv')
+
+if __name__ == "__main__":
+    main()
