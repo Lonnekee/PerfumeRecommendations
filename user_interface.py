b795cef490c08828819f07a6e42ed4dc1a426075
amber-ch <a.chen.1@student.rug.nl>
2020-11-18 11:56:50 +0100

    Add start of GUI

---
+++ user_interface.py
@@ -0,0 +1,64 @@
+try:
+    import Tkinter as tk
+except:
+    import tkinter as tk
+    
+class SampleApp(tk.Tk):
+    def __init__(self):
+      tk.Tk.__init__(self)
+      self.geometry('750x500')
+      self.title('Perfume Knowledge System')
+      self._frame = None
+      self.switch_frame(StartPage)
+      self.first_name = None
+      self.outcome = None
+
+    def switch_frame(self, frame_class):
+      new_frame = frame_class(self)
+      if self._frame is not None:
+          self._frame.destroy()
+      self._frame = new_frame
+      self._frame.pack()
+
+
+class StartPage(tk.Frame):
+  def __init__(self, master):
+      tk.Frame.__init__(self, master)
+      tk.Label(self, text="Welcome to the perfume knowledge system! After you have answered the questions, the system will determine the ideal scented product for your personal use.", wraplength = 750, font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+      tk.Button(self, text="Click here to start",
+                command=lambda: master.switch_frame(PageOne)).pack()
+
+class PageOne(tk.Frame):
+  def __init__(self, master):
+      tk.Frame.__init__(self, master)
+      tk.Label(self, text="What is your name?", wraplength=750, font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+      tk.Label(self, text="Name")
+      name_string = tk.StringVar()
+      name_entry = tk.Entry(self, textvariable = name_string)
+      name_entry.pack()
+      def get_input():
+        first_name = name_string.get()
+        master.first_name.set(first_name)
+      tk.Button(self, text="Submit", command = lambda: get_input).pack()
+      tk.Button(self, text="Next",
+                command=lambda: master.switch_frame(PageTwo)).pack()
+
+class PageTwo(tk.Frame):
+  def __init__(self, master):
+      tk.Frame.__init__(self, master)
+      tk.Label(self, text="Welcome %s" % (master.first_name), font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+      tk.Label(self, text="Do you like the smell of roses?", font=('Helvetica', 12)).pack(side="top", fill="x", pady=5)
+      v = tk.IntVar()
+      tk.Radiobutton(self, text="Yes", variable=v, value=1, indicatoron=0, command=lambda: master.switch_frame(EndPage)).pack()
+      tk.Radiobutton(self, text="No", variable=v, value=2, indicatoron=0, command=lambda: master.switch_frame(EndPage)).pack()
+      
+
+class EndPage(tk.Frame):
+  def __init__(self, master):
+    tk.Frame.__init__(self, master)
+    tk.Label(self, text="Your recommended scent is %s" % (master.outcome), font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+    tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack(side="bottom")
+
+if __name__ == "__main__":
+  app = SampleApp()
+  app.mainloop()
